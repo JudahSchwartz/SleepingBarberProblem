26495dd02b10e3021df61999cbfdaf44220e859c
Judah Schwartz <judahsch@gmail.com>
2018-10-21 11:09:46 -0400

    waiting room now has 3 seats

--- src/Client.java
+++ src/Client.java
@@ -1,12 +1,19 @@
 import java.util.Queue;
+import java.util.Random;
 
 public class Client extends Thread {
 
     Queue<Client> waitingRoom;
+    static int numberOfSeats = 0;
     boolean finished = false;
+    Random random = new Random();
 
     public Client(String s) {
         super(s);
+    }
+
+    public static void setNumberOfSeats(int numberOfSeats) {
+        Client.numberOfSeats = numberOfSeats;
     }
 
     public void setWaitingRoom(Queue<Client> queue)
@@ -17,15 +24,28 @@ public class Client extends Thread {
     public void run()
     {
         try {
-            Thread.sleep((long) (Math.random()*5000));
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        try {
-            synchronized (this) {
-                waitingRoom.add(this);
-                wait();
-            }
+            var added = false;
+            do {
+                synchronized (this) {
+                    synchronized (waitingRoom) {//get lock so seats noone sits while Im deciding to sit
+                        if (waitingRoom.size() < numberOfSeats) {
+                            waitingRoom.add(this);
+                            added = true;
+                            System.out.println(getName() + " sat in waiting room.");
+                            System.out.println(" People in waiting room:  " + waitingRoom.size());
+                        }
+                    }//always release the lock so others can check waiting room
+                    if(added) {
+                        wait();
+                    }
+                    else {
+                        System.out.println(getName() + "no room in waiting room, sleeping");
+                        Thread.sleep(random.nextInt(2000));
+                    }
+
+
+                }
+            }while(!added);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,10 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class Main {
     static final int NUMBER_OF_CLIENTS = 100;
+    static final int NUMBER_OF_SEATS = 3;
     public static void main(String[] args) {
         Barber.setNumberOfClients(NUMBER_OF_CLIENTS);
+        Client.setNumberOfSeats(NUMBER_OF_SEATS);
         System.out.println("Opening barber shop for the day.");
         Queue<Client> waitingRoom = new ConcurrentLinkedQueue<>();
         Barber barberOne = new Barber("Barber #1");
@@ -19,6 +21,7 @@ public class Main {
             client.setWaitingRoom(waitingRoom);
             client.start();
         }
+
 
 
 
